805717bdae874c8b0d1a38f1771182afcc20842c
HoaSec <37164935+HoaSec@users.noreply.github.com>
2024-11-22 10:53:49 -0500

    Add files via upload

---
+++ benign.bat
@@ -0,0 +1,5 @@
+:==========================
+:The below script is used for demo purposes to generate log activity
+:==========================
+ipconfig
+powershell -command "curl https://malware-traffic-analysis.net/ -o innocent.txt"
